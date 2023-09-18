15368257f94d0e107c554462e40db1f084a66f63
hshunt <henry.stewart.hunt@gmail.com>
2023-09-18 14:24:12 -0700

    update

---
+++ Day1/bandnamegenerator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator\n")
+
+cityname = input("What's the name of the city you grew up in?\n")
+
+petname = input("What's your pet's name?\n")
+
+print("Your band name could be " + cityname.capitalize() + " " + petname.capitalize())
